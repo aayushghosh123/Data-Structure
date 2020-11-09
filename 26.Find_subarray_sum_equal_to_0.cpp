2d87d390ef68b34a9ea376149eee4ae56516506c
geekghosh <ghoshaayush208@gmail.com>
2020-11-09 13:23:34 +0530

    Add files via upload

--- 26.Find_subarray_sum_equal_to_0.cpp
+++ 26.Find_subarray_sum_equal_to_0.cpp
@@ -2,7 +2,23 @@
 using namespace std;
 
 void subset_sum(int arr[],int n){
-    
+    int sum=0;
+
+    unordered_set<int>ss;
+
+    for(int i=0;i<n;i++){
+        sum = sum + arr[i];
+
+        if(sum==0 || ss.find(sum) != ss.end()){
+            cout<<"Subarray sum is 0";
+            exit(0);
+        }
+        else{
+            ss.insert(sum);
+        }
+    }
+
+    cout<<"No subarray sum is 0";
 }
 
 int main(){
